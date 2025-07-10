8d05e5f04bbc3c4383d171aa1d1939eebda3b796
hector fernandez <hectorfrdezr@gmail.com>
2025-07-10 14:59:25 -0400

    entrega-F

--- src/pages/CellPhonePage.tsx
+++ src/pages/CellPhonePage.tsx
@@ -2,28 +2,101 @@ import { LuMinus } from "react-icons/lu";
 import { Separator } from "../components/shared/Separator";
 import { formatPrice } from "../helpers";
 import { CiDeliveryTruck } from "react-icons/ci";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { BsChatLeftText } from "react-icons/bs";
 import { ProductDescription } from "../components/OneProduct/ProductDescription";
 import { GridImages } from "../components/OneProduct/GridImages";
+import { useProduct } from "../hooks/products/useProduct";
+import { useEffect, useMemo, useState } from "react";
+import type { variantProduct } from "../interface";
+
+interface Acc{
+    [key:string]:{
+        name: string;
+        storages: string[];
+    }
+}
 
 export const CellPhonePage = () => {
+
+    const {slug} = useParams<{slug:string}>();
+
+    const {product,isLoading,isError} = useProduct(slug || '');
+
+    const [selectedColor, setSelectedColor] = useState<string | null>(null);
+    const [selectedStorage, setSelectedStorage] = useState<string | null>(null);
+    const [selectedVariant, setSelectedVariant] = useState<variantProduct | null>(null);
+
+    const colors = useMemo(() => {
+        return product?.variants.reduce((acc:Acc,variant:variantProduct)=>{
+            const {color,color_name,storage} = variant;
+
+            if(!acc[color]){
+                acc[color] ={
+                    name: color_name,
+                    storages: [],
+                 };
+            }
+            if(!acc[color].storages.includes(storage)){
+                acc[color].storages.push(storage)
+            }
+            return acc;
+        },{} as Acc
+    ) || {};
+    },[product?.variants]);
+
+    //color Prdeterminado
+    const availableColors = Object.keys(colors)
+        useEffect (()=>{
+            if (!selectedColor && availableColors.length > 0){
+                setSelectedColor(availableColors[0]);
+            }
+
+        },[availableColors,selectedColor]);
+
+    // actualizar storage
+    useEffect(() => {
+        if(selectedColor && colors[selectedColor] && !selectedStorage){
+            setSelectedStorage(colors[selectedColor].storages[0]);
+        }
+    },[selectedColor,colors,selectedStorage]);
+
+
+    //obtener la variante seleccionada
+    useEffect(()=>{
+            if(selectedColor && selectedStorage){
+                const variant = product?.variants.find(
+                    variant => 
+                    variant.color === selectedColor &&
+                    variant.storage === selectedStorage);
+             setSelectedVariant(variant as variantProduct)       
+            };
+    },[selectedColor,selectedStorage,product?.variants]);
+    
+    //Stock
+    const IsOutOfStock = selectedVariant?.stock === 0;
+
+    if(!product || isError)return(
+        <div className="felx justify-center items-center h-[80Vh]">
+            <p>Producto no encontrado</p>
+        </div>
+    );
+
     return (<>
     <div className="h-fit flex flex-col md:flex-row gap-16 mt_8">
         <div>
-           <GridImages images={[]}/>
+           <GridImages images={product.images}/>
         </div>
         <div  className="flex-1 space-y-5">
                 <h1 className="text-3xl font-bold tracking-tigth">
-                    Samsung Galaxy s21 ultra 5g
+                   {product.name}
                 </h1>
 
                 <div className="flex gap-5 item-center">
-                    <span className="tracking-wide text-lg font-semibold">{formatPrice(1200)}</span>
+                    <span className="tracking-wide text-lg font-semibold">{formatPrice(selectedVariant?.price || product.variants[0].price)}</span>
                     
                 <div className="relative">
-                    {/*Agotado*/}
-                    <span>Agotado</span>
+                    {IsOutOfStock && <span>Agotado</span>}
                 </div>
             </div>
 
@@ -31,35 +104,56 @@ export const CellPhonePage = () => {
 
             {/*Caracteristicas */}
              <ul className="space-y-2 ml-7 my-10">
-                <li className="text-sm flex items-center gap-3 tracking-tight font-medium">
+               {
+                product.features.map(feature =>( 
+                <li 
+                key={feature}
+                className="text-sm flex items-center gap-3 tracking-tight font-medium">
                     <span className="bg-black w-[5px] h-[5px] rounded-full"/>
-                    256gb almacenamiento
-                </li>
+                    {feature}
+                </li>))
+               }
              </ul>
 
             <div className="flex flex-col gap-3">
-                    <p>color azul</p>
+                    <p>color:{selectedColor && colors[selectedColor].name}</p>
                     <div className="flex gap-3">
-                        <button className={`w-8 h-8 rounded-full flex justify-center items-center ${ true ? 'bg-blue-500' : 'bg-gray-500'}`}>
+                        {
+                            availableColors.map(color =>(
+                            <button 
+                            key={color}
+                            className={`w-8 h-8 rounded-full flex justify-center items-center ${ selectedColor === color? 'bg-blue-500' : 'bg-gray-500'}`}>
                             <span className="w-[26px] h-[26px] rounded-full" style={{backgroundColor:'blue'}}/>
-                        </button>
+                        </button>))
+                        }
+                        
                     </div>
-            </div>
+            </div> 
 
         {/*Opciones de almacenamiento*/}
         <div className="flex flex-col gap-3">
             <p className="text-xs font-medium">
                 Almacenamiento disponible
             </p>
-            <div className="flex gap-3">
-                <select className="border border-gray-300 rounded-lg px-3 py-1">
-                    <option value="">256Gb</option>
+            {
+                selectedStorage &&(
+                    <div className="flex gap-3">
+                <select className="border border-gray-300 rounded-lg px-3 py-1" value={selectedStorage || ''}
+                    onChange={e => setSelectedStorage(e.target.value)}
+                >
+                    {
+                        colors [selectedColor].storages.map(storage =>(
+                            <option value={storage} key={storage}>{storage}</option>
+                        ))    
+                    
+                    }
                 </select>
-            </div>    
+            </div> 
+                )}   
         </div>
         {/*Comprar*/}
         {
-            false ? (
+            IsOutOfStock ? (
                 <button
                 className="bg-[#f3f3f3] uppercase font-semibold tracking-widest text-xs py-4 rounded-full transition-all duration-300 hover:bg[#e2e2e2] w-full"
                 disabled>
@@ -115,7 +209,7 @@ export const CellPhonePage = () => {
         </div>
     </div>
                 {/**Descripción */}
-    <ProductDescription/>                
+    <ProductDescription content={product.description}/>                
   </>
   );
 };
